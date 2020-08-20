4e0bffae230efbda817b86eca438db7da8b5c103
razvanrosa <razvan_rosa@yahoo.com>
2020-08-20 21:31:31 +0300

    added yaml configuration example

---
+++ src/main/java/org/fasttrackit/onlineshop/domain/User.java
@@ -0,0 +1,20 @@
+package org.fasttrackit.onlineshop.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @NotNull
+    private String firstName;
+    @NotNull
+    private String lastName;
+
+}
