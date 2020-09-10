7d381e080367654170714e81f43cffbb01dbfbf8
razvanrosa <razvan_rosa@yahoo.com>
2020-09-10 19:13:44 +0300

    added tests for getUser method

--- src/test/java/org/fasttrackit/onlineshop/user/UserServiceIntegrationTest.java
+++ src/test/java/org/fasttrackit/onlineshop/user/UserServiceIntegrationTest.java
@@ -1,8 +1,10 @@
 package org.fasttrackit.onlineshop.user;
 
 import org.fasttrackit.onlineshop.domain.User;
+import org.fasttrackit.onlineshop.exception.ResourceNotFoundException;
 import org.fasttrackit.onlineshop.service.UserService;
 import org.fasttrackit.onlineshop.transfer.SaveUserRequest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,17 +24,7 @@ public class UserServiceIntegrationTest {
 
     @Test
     public void createUser_whenValidRequest_thenReturnSavedUser(){
-        SaveUserRequest request = new SaveUserRequest();
-
-        request.setFirstName("Test First Name");
-        request.setLastName("Test Last Name");
-
-        User user = userService.createUser(request);
-
-        assertThat(user, notNullValue());
-        assertThat(user.getId(),greaterThan(0L));
-        assertThat(user.getFirstName(), is(request.getFirstName()));
-        assertThat(user.getLastName(), is(request.getLastName()));
+        createUser();
 
     }
 
@@ -50,5 +42,41 @@ public class UserServiceIntegrationTest {
         }
         assertThat(exception,notNullValue());
         assertThat("Unexpected exception type.",exception instanceof TransactionSystemException);
+    }
+
+    @Test
+    public void getUser_whenExistingUser_thenReturnUser(){
+
+        User createdUser = createUser();
+
+        User userResponse = userService.getUser(createdUser.getId());
+
+        assertThat(userResponse, notNullValue());
+        assertThat(userResponse.getId(),is(createdUser.getId()));
+        assertThat(userResponse.getFirstName(),is(createdUser.getFirstName()));
+        assertThat(userResponse.getLastName(),is(createdUser.getLastName()));
+    }
+
+    @Test
+    public void getUser_whenNonExistingUser_thenThrowResourceNotFoundException(){
+        Assertions.assertThrows(ResourceNotFoundException.class,
+                () ->userService.getUser(99999));
+
+    }
+
+    private User createUser() {
+        SaveUserRequest request = new SaveUserRequest();
+
+        request.setFirstName("Test First Name");
+        request.setLastName("Test Last Name");
+
+        User user = userService.createUser(request);
+
+        assertThat(user, notNullValue());
+        assertThat(user.getId(),greaterThan(0L));
+        assertThat(user.getFirstName(), is(request.getFirstName()));
+        assertThat(user.getLastName(), is(request.getLastName()));
+
+        return user;
     }
 }
